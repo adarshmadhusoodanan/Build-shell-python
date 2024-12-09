f600c44ee1ec57e054cba41168e05e85dfec16a4
Adarsh Madhusoodanan <iadarshmadhusoodanan@gmail.com>
2024-12-10 01:06:16 +0530

    [any messsge]

--- app/main.py
+++ app/main.py
@@ -6,14 +6,14 @@ def main():
 
 
     valid_commands = []
-    
+
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
         # Wait for user input
         user_command = input()
         if user_command not in valid_commands:
-            print(f"${user_command}: command not found")
+            print(f"{user_command}: command not found")
             continue
 
 if __name__ == "__main__":
