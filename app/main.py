69b7bc351c0b9334f9b4126eadbe498d90b5b4c4
Adarsh Madhusoodanan <iadarshmadhusoodanan@gmail.com>
2024-12-21 12:17:23 +0530

    [any messsge]

--- app/main.py
+++ app/main.py
@@ -17,6 +17,16 @@ def main():
         if args[0] == "exit":
             if args[1] == "0":
                 sys.exit(0)
+        elif args[0] == "type":
+            if args[1] == "echo":
+                print("echo is a shell builtin")
+            elif args[1] == "exit":
+                print("exit is a shell builtin")
+            elif args[1] == "type":
+                print("type is a shell builtin")
+            else:
+                invalid = user_command[len("type "):]
+                print(f"{invalid}: not found")
         elif args[0] == "echo":
             print(user_command[len("echo "):])
         else:
